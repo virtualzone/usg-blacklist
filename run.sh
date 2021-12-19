7b56d9b41d64966c5b42f72e2abe8bbe2585250a
virtualzone <13085858+virtualzone@users.noreply.github.com>
2021-12-19 08:07:22 +0100

    Added RSA as accepted key type

--- run.sh
+++ run.sh
@@ -5,10 +5,10 @@ echo "" >> /root/.ssh/id_rsa
 echo "" >> /root/.ssh/id_rsa.pub
 chmod 400 /root/.ssh/id_rsa*
 if [ "$1" = "stats" ]; then
-    ssh -o StrictHostKeyChecking=no -o PasswordAuthentication=no $3@$2 'bash -s' < /opt/usg-blacklist/stats.sh
+    ssh -o StrictHostKeyChecking=no -o PubkeyAcceptedKeyTypes=ssh-rsa -o PasswordAuthentication=no $3@$2 'bash -s' < /opt/usg-blacklist/stats.sh
 elif [ "$4" = "true" ]; then
-    ssh -o StrictHostKeyChecking=no -o PasswordAuthentication=no $3@$2 'bash -s' < /opt/usg-blacklist/blacklist.sh
-    ssh -o StrictHostKeyChecking=no -o PasswordAuthentication=no $3@$2 'bash -s' < /opt/usg-blacklist/blacklist6.sh
+    ssh -o StrictHostKeyChecking=no -o PubkeyAcceptedKeyTypes=ssh-rsa -o PasswordAuthentication=no $3@$2 'bash -s' < /opt/usg-blacklist/blacklist.sh
+    ssh -o StrictHostKeyChecking=no -o PubkeyAcceptedKeyTypes=ssh-rsa -o PasswordAuthentication=no $3@$2 'bash -s' < /opt/usg-blacklist/blacklist6.sh
 else
-    ssh -o StrictHostKeyChecking=no -o PasswordAuthentication=no $3@$2 'bash -s' < /opt/usg-blacklist/blacklist.sh
+    ssh -o StrictHostKeyChecking=no -o PubkeyAcceptedKeyTypes=ssh-rsa -o PasswordAuthentication=no $3@$2 'bash -s' < /opt/usg-blacklist/blacklist.sh
 fi
