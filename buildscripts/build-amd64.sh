ef56758176d7f89f66abe72c5f2face4e6e1b6b8
virtualzone <13085858+virtualzone@users.noreply.github.com>
2021-12-19 09:04:56 +0100

    Removed obsolete files

--- buildscripts/build-amd64.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-docker build --pull -t virtualzone/usg-blacklist:amd64 -t virtualzone/usg-blacklist:latest ../.
-docker push virtualzone/usg-blacklist:amd64
-docker push virtualzone/usg-blacklist:latest
--- buildscripts/build-arm64.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-docker build --pull -t virtualzone/usg-blacklist:arm64 ../.
-docker push virtualzone/usg-blacklist:arm64
